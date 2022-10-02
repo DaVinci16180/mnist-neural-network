36b17eb332e3d85eb23c8b754259fafc7473eb20
Davi Guimarães <ddaavvii2001@gmail.com>
2022-10-02 09:59:50 -0300

    --

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ from neural_network import get_formatted_img_from_path, gradient_descent, make_p
 
 W1, b1, W2, b2 = gradient_descent(0.10, 1000)
 
-x = get_formatted_img_from_path('')
+# x = get_formatted_img_from_path('')
 
-print(make_predictions(x, W1, b1, W2, b2))
+# print(make_predictions(x, W1, b1, W2, b2))
+
+test_prediction(1, W1, b1, W2, b2)
