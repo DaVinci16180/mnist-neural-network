aa3c9d49898fb2be04dd314ebd1b8ef03b474346
Davi Guimarães <ddaavvii2001@gmail.com>
2022-10-02 10:04:56 -0300

    adding model folder

--- neural_network.py
+++ neural_network.py
@@ -8,16 +8,10 @@ data = np.array(data)
 m, n = data.shape
 np.random.shuffle(data)
 
-data_dev = data[0:1000].T
-Y_dev = data_dev[0]
-X_dev = data_dev[1:n]
-X_dev = X_dev / 255.
-
 data_train = data[1000:m].T
 Y_train = data_train[0]
 X_train = data_train[1:n]
 X_train = X_train / 255.
-_,m_train = X_train.shape
 
 def init_params():
     W1 = np.random.rand(10, 784) - 0.5
