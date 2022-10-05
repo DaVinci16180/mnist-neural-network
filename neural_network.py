0845e8dc26374a66645331819d35cea07aba8a51
Davi Guimarães <ddaavvii2001@gmail.com>
2022-10-04 21:50:17 -0300

    --

--- neural_network.py
+++ neural_network.py
@@ -5,6 +5,7 @@ import cv2
 data = pd.read_csv('csv/train.csv')
 
 data = np.array(data)
+
 m, n = data.shape
 np.random.shuffle(data)
 
@@ -16,8 +17,10 @@ X_train = X_train / 255.
 def init_params():
     W1 = np.random.rand(10, 784) - 0.5
     b1 = np.random.rand(10, 1) - 0.5
+
     W2 = np.random.rand(10, 10) - 0.5
     b2 = np.random.rand(10, 1) - 0.5
+
     return W1, b1, W2, b2
 
 def ReLU(Z):
@@ -33,15 +36,11 @@ def softmax(Z):
 def feed_forward(W1, b1, W2, b2, X):
     Z1 = W1.dot(X) + b1
     A1 = ReLU(Z1)
+
     Z2 = W2.dot(A1) + b2
     A2 = softmax(Z2)
-    return Z1, A1, Z2, A2
 
-def one_hot(Y):
-    one_hot_Y = np.zeros((Y.size, Y.max() + 1))
-    one_hot_Y[np.arange(Y.size), Y] = 1
-    one_hot_Y = one_hot_Y.T
-    return one_hot_Y
+    return Z1, A1, Z2, A2
 
 def back_propagation(Z1, A1, Z2, A2, W1, W2, X, Y):
     one_hot_Y = one_hot(Y)
@@ -56,8 +55,10 @@ def back_propagation(Z1, A1, Z2, A2, W1, W2, X, Y):
 def update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha):
     W1 = W1 - alpha * dW1
     b1 = b1 - alpha * db1
+
     W2 = W2 - alpha * dW2
     b2 = b2 - alpha * db2
+
     return W1, b1, W2, b2
 
 def gradient_descent(alpha, iterations):
@@ -74,10 +75,11 @@ def gradient_descent(alpha, iterations):
             Z1, A1, Z2, A2 = feed_forward(W1, b1, W2, b2, X_train)
             dW1, db1, dW2, db2 = back_propagation(Z1, A1, Z2, A2, W1, W2, X_train, Y_train)
             W1, b1, W2, b2 = update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha)
-            if i % 100 == 0:
+
+            if i % 10 == 0:
                 print("Iteration: ", i)
                 predictions = get_predictions(A2)
-                print(get_accuracy(predictions, Y_train))
+                print("Accuracy: ", get_accuracy(predictions, Y_train))
 
         np.save('model/w1.npy', W1)
         np.save('model/b1.npy', b1)
@@ -90,7 +92,6 @@ def get_predictions(A2):
     return np.argmax(A2, 0)
 
 def get_accuracy(predictions, Y):
-    print(predictions, Y)
     return np.sum(predictions == Y) / Y.size
 
 def make_predictions(X, W1, b1, W2, b2):
@@ -102,6 +103,7 @@ def test_prediction(index, W1, b1, W2, b2):
     current_image = X_train[:, index, None]
     prediction = make_predictions(X_train[:, index, None], W1, b1, W2, b2)
     label = Y_train[index]
+    
     print("Prediction: ", prediction)
     print("Label: ", label)
     
@@ -117,3 +119,17 @@ def get_formatted_img_from_path(path):
         flat.append([i])
 
     return np.array(flat)
+
+def translate_output(out):
+    if (out == 6):
+        print('Primeiro número.')
+    elif (out == 4):
+        print('Segundo número.')
+    else:
+        print('Número não reconhecido.')
+
+def one_hot(Y):
+    one_hot_Y = np.zeros((Y.size, Y.max() + 1))
+    one_hot_Y[np.arange(Y.size), Y] = 1
+    one_hot_Y = one_hot_Y.T
+    return one_hot_Y
--- test.py
+++ test.py
@@ -1,9 +1,12 @@
-from neural_network import get_formatted_img_from_path, gradient_descent, make_predictions, test_prediction
+from neural_network import get_formatted_img_from_path, gradient_descent, make_predictions, test_prediction, translate_output
 
 W1, b1, W2, b2 = gradient_descent(0.10, 1000)
 
-# x = get_formatted_img_from_path('')
+x = get_formatted_img_from_path('1.png')
+saida = make_predictions(x, W1, b1, W2, b2)
 
-# print(make_predictions(x, W1, b1, W2, b2))
+print(saida)
+translate_output(saida)
 
-test_prediction(1, W1, b1, W2, b2)
+# Davi: 6
+# William: 4
